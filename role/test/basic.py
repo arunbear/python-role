16c6467e95aa8eb81bc24c31ce3de69521baf5f3
Arun Prasaad <arunbear@gmail.com>
2014-04-20 19:17:17 +0100

    Test consumtion of two roles.

--- role/test/basic.py
+++ role/test/basic.py
@@ -1,5 +1,6 @@
 import unittest
 import role.test.messenger
+import role.test.passenger
 
 from role import consume
 
@@ -14,6 +15,16 @@ class RoleTester(unittest.TestCase):
         self.assertEqual(obj.how(), 'I am fine')
         self.assertEqual(obj.bye(), 'bye bye world')
 
+    def test_consume_two_roles(self):
+        @consume(role.test.messenger, role.test.passenger)
+        class Foo(object):
+            def how(self):
+                return 'I am fine'
+        obj = Foo()
+        self.assertEqual(obj.hello(), 'hello world')
+        self.assertEqual(obj.introduce(), 'I am the passenger')
+        self.assertEqual(obj.how(), 'I am fine')
+        self.assertEqual(obj.bye(), 'bye bye world')
 
 if __name__ == '__main__':
     unittest.main()
