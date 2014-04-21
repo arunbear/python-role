64f23ba3d57bb8b5818ef04555164f379e18cdf4
Arun Prasaad <arunbear@gmail.com>
2014-04-21 19:13:55 +0100

    Required methods

--- role/__init__.py
+++ role/__init__.py
@@ -1,10 +1,12 @@
-from inspect import getmembers, isfunction
+from inspect import getmembers, isfunction, ismethod
 
 def consume(*roles):
     def deco(cls):
         provided_by = {}
         for role in roles:
             _add_methods(role, cls, provided_by)
+        for role in roles:
+            _chk_requirements(role, cls)
         return cls
     return deco
 
@@ -16,6 +18,11 @@ def _add_methods(role, cls, provided_by):
         elif attr in provided_by:
             raise _conflict_error(attr, role, provided_by)
 
+def _chk_requirements(role, cls):
+    for meth in getattr(role, 'requires', ()):
+        if not ismethod(getattr(cls, meth, None)):
+            raise _requirement_error(cls, meth, role)
+
 def _conflict_error(attr, role, provided_by):
     msg = 'Method %s.%s conflicts with %s.%s' % (
         role.__name__,
@@ -24,3 +31,11 @@ def _conflict_error(attr, role, provided_by):
         attr,
     )
     return NameError(msg)
+
+def _requirement_error(cls, meth, role):
+    msg = '%s does not have a %s method, which is required by %s' % (
+        cls.__name__,
+        meth,
+        role.__name__,
+    )
+    return TypeError(msg)
